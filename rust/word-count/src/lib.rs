25a2550748ac7b826494555fbfd25ebb105e9eb2
Eric Wustrow <ewust@umich.edu>
2016-02-18 01:04:05 -0700

    this works, you can really only use hashmaps if you use entry with it

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,17 +1,15 @@
 use std::collections::HashMap;
 
-pub fn word_count<'a>(s: &'a str) -> HashMap<String, u32> {
+pub fn word_count(s: &str) -> HashMap<String, u32> {
 
-    let mut m: HashMap<String, u32> = HashMap::new();
+    let mut m = HashMap::new();
 
     for word in s.split(|c: char| !(c.is_alphabetic() || c.is_numeric()))
                     .filter(|s| s.len() != 0)
-                    .map(|s| s.to_string()) {
+                    .map(|s| s.to_string().to_lowercase()) {
 
-        match m.get(&word) {
-            Some(&count) => m.insert(word, count + 1),
-            None => m.insert(word, 1)
-        }
+        let counter = m.entry(word).or_insert(0);
+        *counter += 1;
     }
     m
 }
