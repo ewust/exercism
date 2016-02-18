0aa46fcd3fdbac7f5564bf2e5333d8c441363099
Eric Wustrow <ewust@umich.edu>
2016-02-18 01:10:46 -0700

    what have i done

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,14 +1,11 @@
 use std::collections::HashMap;
 
 pub fn word_count(s: &str) -> HashMap<String, u32> {
-
-    let mut m = HashMap::new();
-
-    for word in s.split(|c: char| !c.is_alphanumeric())
-                    .filter(|s| s.len() != 0)
-                    .map(|s| s.to_string().to_lowercase()) {
-
-        *m.entry(word).or_insert(0) += 1;
-    }
-    m
+    s.split(|c: char| !c.is_alphanumeric())
+        .filter(|s| s.len() != 0)
+        .map(|s| s.to_string().to_lowercase())
+        .fold(HashMap::new(), |mut m, word| {
+            *m.entry(word).or_insert(0) += 1;
+            m
+        })
 }
