c33bcb56cc24ee6008daa8db41d3378b32952231
Eric Wustrow <ewust@umich.edu>
2016-02-18 01:08:46 -0700

    more concise entry stuff

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -4,12 +4,11 @@ pub fn word_count(s: &str) -> HashMap<String, u32> {
 
     let mut m = HashMap::new();
 
-    for word in s.split(|c: char| !(c.is_alphabetic() || c.is_numeric()))
+    for word in s.split(|c: char| !c.is_alphanumeric())
                     .filter(|s| s.len() != 0)
                     .map(|s| s.to_string().to_lowercase()) {
 
-        let counter = m.entry(word).or_insert(0);
-        *counter += 1;
+        *m.entry(word).or_insert(0) += 1;
     }
     m
 }
