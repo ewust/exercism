d4d0fc239cafb6dc4f51a11c0d8a9b0c8173b3f2
Eric Wustrow <ewust@colorado.edu>
2016-02-18 15:16:25 -0700

    rna transcription

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,12 +1,41 @@
 
+#[derive(PartialEq,Debug)]
 pub struct RibonucleicAcid {
     strand : String,
 }
 
+#[derive(PartialEq,Debug)]
+pub struct DeoxyribonucleicAcid {
+    strand: String,
+}
+
 impl RibonucleicAcid {
-    pub fn new(s: &str) {
-        RibonucleicAcid { strand: s }
+    pub fn new(s: &str) -> RibonucleicAcid {
+        RibonucleicAcid { strand: s.to_string() }
+    }
+
+    pub fn as_ref(&self) -> String {
+        self.strand.clone()
     }
 }
 
+impl DeoxyribonucleicAcid {
+    pub fn new(s: &str) -> DeoxyribonucleicAcid {
+        DeoxyribonucleicAcid { strand: s.to_string() }
+    }
 
+    pub fn to_rna(&self) -> RibonucleicAcid {
+
+        RibonucleicAcid { strand:
+            self.strand.clone()
+                .chars()
+                .map(|c| match c {
+                    'G' => 'C',
+                    'C' => 'G',
+                    'T' => 'A',
+                    'A' => 'U',
+                    _ => '?',
+                }).collect()
+            }
+    }
+}
