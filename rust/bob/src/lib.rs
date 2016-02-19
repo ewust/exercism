68ee845242abbebd35df50ed4b065e099a520e1b
Eric Wustrow <ewust@colorado.edu>
2016-02-19 12:49:52 -0700

    bob with traits

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,13 +1,25 @@
+trait Bob {
+    fn is_shouting(&self) -> bool;
+    fn is_silent(&self) -> bool;
+}
+
+impl Bob for str {
+    fn is_shouting(&self) -> bool {
+        self.chars().all(|c| (!c.is_alphabetic() || c.is_uppercase()))
+    }
+
+    fn is_silent(&self) -> bool {
+        self.chars().all(|c| !c.is_whitespace())
+    }
+}
 
 
 pub fn reply(s: &str) -> &'static str {
     if s.ends_with("?") {
         "Sure."
-    } else if s.is_empty() {
+    } else if s.is_silent() {
         "Fine. Be that way!"
-    } else if s.chars().fold(true, |mut acc, c| {
-      acc &= (!c.is_alphabetic() || c.is_uppercase());
-      acc }) {
+    } else if s.is_shouting() {
         "Whoa, chill out!"
     } else {
         "Whatever."
